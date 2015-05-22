a2b4b4f9019b4302a05de6683335639dc0cd1cbc
shimanyi <vshimanyi@gmail.com>
2015-05-22 11:44:44 +0300

    added a working version of sessions

--- bootstrap-3.3.4-dist/start2.php
+++ bootstrap-3.3.4-dist/start2.php
@@ -1,3 +1,7 @@
+<?php
+	session_start();
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head> 
@@ -18,16 +22,16 @@
 <div class="form-group">
 	<label for="name" class="col-sm-2 control-label">Name:</label>
 		<div class="col-sm-7">			
-		 	<input type="text" class="form-control" id="name" placeholder="first and lastname" 
-		 		value="<?php if(isset($_POST['name'])){	echo $_POST['name']; }?>">
-		 
+		 	<input type="text" class="form-control" id="name" name="firstname" placeholder="first and lastname" 
+		 		value="<?php echo $_SESSION['name'];?>">
+		 		
 	</div>
 </div>
 
 <div class="form-group">
 	<label for="email" class="col-sm-2 control-label">Email:</label>
 		<div class="col-sm-7">								
-			<input type="text" class="form-control" id="email" placeholder="example@yahoo.com" value="">
+			<input type="text" class="form-control" name="email" placeholder="example@yahoo.com" value="<?php echo $_SESSION['email'];?>">
 		
 	</div>
 </div>
@@ -35,14 +39,14 @@
 <div class="form-group">
 	<label for="password" class="col-sm-2 control-label">Password:</label>
 		<div class="col-sm-7">
-			<input type="password" class="form-control" id="password" placeholder="enter your password" value="">
+			<input type="password" class="form-control" name="password" placeholder="enter your password" value="">
 	</div>
 </div>
 
 <div class="form-group">
 	<label for="repassword" class="col-sm-2 control-label">Repassword:</label>
 	<div class="col-sm-7">
-		<input type="password" class="form-control" id="repassword" placeholder="enter your password" value="">
+		<input type="password" class="form-control" name="repassword" placeholder="enter your password" value="">
 		
 	</div>
 </div>
